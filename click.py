e8cb01571716c43b0f1e7f0744255217049f71e3
kj5377701 <kj5377701@gmail.com>
2021-12-14 11:55:48 +0800

    12/14

--- click.py
+++ click.py
@@ -1,17 +1,24 @@
 import pyautogui as auto
 import win32api
 import time
+import csv
 
-state = win32api.GetKeyState(0x01)
-while True:
-    left = win32api.GetKeyState(0x01)
-    if left != state:
-        state = left
-        print(left)
-        if left < 0:
-            print("press")
-            print(auto.position())
-        else:
-            print("release")
-    time.sleep(0.01)
-
+with open("data.csv", "w", newline='') as file:
+    writer = csv.writer(file)
+    dataset = []
+    j = 1
+    state = win32api.GetKeyState(0x01)
+    while True:
+        position = [j]
+        for i in range(3):
+            left = win32api.GetKeyState(0x01)
+            if left != state:
+                state = left
+                if left < 0:
+                    print("press")
+                    position.append(auto.position())
+            else:
+                continue
+            time.sleep(0.1)
+        writer.writerow(position)
+        j += 1
